7d929bfcfe78ea51595cdf9abf421f7b17844861
Tyler Beringer <99tylerberinger@gmail.com>
2021-03-20 13:21:10 -0400

    Made two methods for all textwidget editing

--- src/main.py
+++ src/main.py
@@ -42,7 +42,7 @@ class Rebalancer():
     
     def calculate(self):
 
-        self.clear_texterror()
+        self.textwidget_clear(self.text_error)
 
         # Get all input values and verify them
         stocks = {}
@@ -58,49 +58,46 @@ class Rebalancer():
                     stock_value = self.get_float_from_string(things[1])
                     total_stock_value += stock_value
                 except ValueError as e:
-                    self.insert_texterror(things[1])
+                    self.textwidget_insert(self.text_error, things[1])
                     return
                 try:
                     stock_desired_percent = self.get_float_from_string(things[2])
                     total_stock_desired_percent += stock_desired_percent
                 except ValueError as e:
-                    self.insert_texterror(things[2])
+                    self.textwidget_insert(self.text_error, things[2])
                     return
                 stocks[stock] = [stock_value, stock_desired_percent]
             else:
-                self.insert_texterror(f"'{text_input_line}'")
+                self.textwidget_insert(self.text_error, f"'{text_input_line}'")
                 return
         if total_stock_desired_percent != 100.0:
-            self.insert_texterror(f"total_stock_desired_percent={total_stock_desired_percent} (should be 100.0)")
+            self.textwidget_insert(self.text_error, f"total_stock_desired_percent={total_stock_desired_percent} (should be 100.0)")
             return
         try:
             amount_to_invest = self.get_float_from_string(self.stringvar_amounttoinvest.get())
         except ValueError as e:
-            self.insert_texterror("invalid amount_to_invest")
+            self.textwidget_insert(self.text_error, "invalid amount_to_invest")
             return
         
         # Use the input values to calculate and display the investment split
         new_total_stock_value = total_stock_value + amount_to_invest
-        self.text_output.configure(state=tk.NORMAL)
         for stock in stocks:
             needed_value = (stocks[stock][1] / 100.0) * new_total_stock_value
             difference = needed_value - stocks[stock][0]
-            self.text_output.insert(tk.END, f"Put {difference} in {stock}\n")
-        self.text_output.configure(state=tk.DISABLED)
+            self.textwidget_insert(self.text_output, f"Put {difference} in {stock}")
     
     def get_float_from_string(self, string):
         return float("".join([char for char in string if char.isdigit() or char == "."]))
     
-    def insert_texterror(self, error):
-        self.clear_texterror()
-        self.text_error.configure(state=tk.NORMAL)
-        self.text_error.insert(tk.END, f"ERROR: {error}")
-        self.text_error.configure(state=tk.DISABLED)
+    def textwidget_insert(self, textwidget, text):
+        textwidget.configure(state=tk.NORMAL)
+        textwidget.insert(tk.END, f"{text}\n")
+        textwidget.configure(state=tk.DISABLED)
     
-    def clear_texterror(self):
-        self.text_error.configure(state=tk.NORMAL)
-        self.text_error.delete("1.0", tk.END)
-        self.text_error.configure(state=tk.DISABLED)
+    def textwidget_clear(self, textwidget):
+        textwidget.configure(state=tk.NORMAL)
+        textwidget.delete("1.0", tk.END)
+        textwidget.configure(state=tk.DISABLED)
 
 
 Rebalancer()

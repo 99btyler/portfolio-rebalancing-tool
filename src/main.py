c7441a96656b1da375bc41e86ea32db5914954e8
Tyler Beringer <99tylerberinger@gmail.com>
2021-03-22 18:16:38 -0400

    Changed style

--- src/main.py
+++ src/main.py
@@ -31,7 +31,7 @@ class Rebalancer():
         self.entry_amounttoinvest = ttk.Entry(self.frame, textvariable=self.stringvar_amounttoinvest)
         self.entry_amounttoinvest.grid()
 
-        self.button_calculate = ttk.Button(self.frame, text="Calculate", command=self.calculate)
+        self.button_calculate = ttk.Button(self.frame, text="Calculate", command=self.__calculate)
         self.button_calculate.grid()
 
         self.text_output = tk.Text(self.frame, height=3, state=tk.DISABLED)
@@ -40,10 +40,10 @@ class Rebalancer():
         # Tkinter mainloop
         self.root.mainloop()
     
-    def calculate(self):
+    def __calculate(self):
 
-        self.textwidget_clear(self.text_error)
-        self.textwidget_clear(self.text_output)
+        self.__textwidget_clear(self.text_error)
+        self.__textwidget_clear(self.text_output)
 
         # Get all input values and verify them
         stocks = {}
@@ -56,28 +56,28 @@ class Rebalancer():
             if len(things) == 3:
                 stock = things[0]
                 try:
-                    stock_value = self.get_float_from_string(things[1])
+                    stock_value = self.__get_float_from_string(things[1])
                     total_stock_value += stock_value
                 except ValueError as e:
-                    self.textwidget_insert(self.text_error, things[1])
+                    self.__textwidget_insert(self.text_error, things[1])
                     return
                 try:
-                    stock_desired_percent = self.get_float_from_string(things[2])
+                    stock_desired_percent = self.__get_float_from_string(things[2])
                     total_stock_desired_percent += stock_desired_percent
                 except ValueError as e:
-                    self.textwidget_insert(self.text_error, things[2])
+                    self.__textwidget_insert(self.text_error, things[2])
                     return
                 stocks[stock] = [stock_value, stock_desired_percent]
             else:
-                self.textwidget_insert(self.text_error, f"'{text_input_line}'")
+                self.__textwidget_insert(self.text_error, f"'{text_input_line}'")
                 return
         if total_stock_desired_percent != 100.0:
-            self.textwidget_insert(self.text_error, f"total_stock_desired_percent={total_stock_desired_percent} (should be 100.0)")
+            self.__textwidget_insert(self.text_error, f"total_stock_desired_percent={total_stock_desired_percent} (should be 100.0)")
             return
         try:
-            amount_to_invest = self.get_float_from_string(self.stringvar_amounttoinvest.get())
+            amount_to_invest = self.__get_float_from_string(self.stringvar_amounttoinvest.get())
         except ValueError as e:
-            self.textwidget_insert(self.text_error, "invalid amount_to_invest")
+            self.__textwidget_insert(self.text_error, "invalid amount_to_invest")
             return
         
         # Use the input values to calculate and display the investment split
@@ -85,20 +85,22 @@ class Rebalancer():
         for stock in stocks:
             needed_value = (stocks[stock][1] / 100.0) * new_total_stock_value
             difference = needed_value - stocks[stock][0]
-            self.textwidget_insert(self.text_output, f"Put {round(difference, 2)} in {stock}")
+            self.__textwidget_insert(self.text_output, f"Put {round(difference, 2)} in {stock}")
     
-    def textwidget_insert(self, textwidget, text):
+    def __textwidget_insert(self, textwidget, text):
         textwidget.configure(state=tk.NORMAL)
         textwidget.insert(tk.END, f"{text}\n")
         textwidget.configure(state=tk.DISABLED)
     
-    def textwidget_clear(self, textwidget):
+    def __textwidget_clear(self, textwidget):
         textwidget.configure(state=tk.NORMAL)
         textwidget.delete("1.0", tk.END)
         textwidget.configure(state=tk.DISABLED)
     
-    def get_float_from_string(self, string):
+    def __get_float_from_string(self, string):
         return float("".join([char for char in string if char.isdigit() or char == "."]))
 
 
-Rebalancer()
+if __name__ == "__main__":
+    Rebalancer()
+
